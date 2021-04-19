85e6a488436b7e2df697cb863c71021dcf50ee64
Red3ffect <red3ffect117@gmail.com>
2021-04-18 23:43:48 -0700

    Added partial audit task function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,15 @@ console.log(moment().format("dddd, MMMM Do YYYY"));
 appointments = [];
 
 
-var createTask = function (appointmentHour, appointmentTask) {
-    //debugger;
+var createTasks = function (appointmentHour, appointmentTask) {
     var taskDiv = $("<div>").attr("id","appointment").addClass("row time-block");
-    var taskP = $("<p>").addClass("col hour").text(appointmentHour);
-    var taskDesc = $("<textarea>").addClass("col-8 textarea description").text(appointmentTask);
-    var saveBtn = $("<button>").addClass("col save-btn").text("Save");
+    var taskP = $("<p>").attr("id", "hour").addClass("col hour").text(appointmentHour);
+    var taskDesc = $("<textarea>").attr("id","task").addClass("col-8 textarea description future").text(appointmentTask);
+    var saveBtn = $("<button>").addClass("col saveBtn").text("Save");
     taskDiv.append(taskP, taskDesc, saveBtn);
+
+    // auditTasks(taskDiv);
+
     $("#container").append(taskDiv);
 };
 
@@ -38,16 +40,30 @@ var loadTasks = function () {
     console.log(appointments);
 
     $.each(appointments, function (list, arr) {
-        //debugger;
         console.log(list, arr);
-        //createTask(appointments.hour, appointments.task);
 
         arr.forEach(function (appointments) {
-            //debugger;
-            createTask(appointments.hour, appointments.task);
+            createTasks(appointments.hour, appointments.task);
         });
     });
 };
+
+var saveTasks = function(){
+    localStorage.setItem("appointments", JSON.stringify(appointments));
+};
+
+// var auditTasks = function(taskEl) {
+//     var time = $(taskEl).find("p").text().trim();
+//     var schedTime = moment(time, "LT");
+
+//     console.log(shedTime);
+
+// //     $(taskEl).find("textarea").removeClass("future");
+
+// //     if (moment().isAfter())
+
+// //     console.log(currentTime);
+// };
 
 
 
@@ -64,7 +80,7 @@ $("#container").on("click", "textarea", function(){
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
     console.log(text, textInput);
-})
+});
 
 
 $("#container").on("blur", "textarea", function(){
@@ -86,57 +102,11 @@ $("#container").on("blur", "textarea", function(){
 });
 
 $(".time-block").on("click", "button", function(){
-    localStorage.setItem("appointments", JSON.stringify(appointments));
-    console.log("text");
+    saveTasks();
 });
 
-
-// localStorage.setItem("schedule", JSON.stringify(schedule));
-
-// console.log(schedule);
-
-// for (var i = 0; i < schedule.hour.length; i++) {
-//     var taskDiv = $("<div>").addClass("row time-block");
-//     var taskP = $("<p>").addClass("col hour").text(schedule.hour[i] + " am");
-//     var taskDesc = $("<textarea>").addClass("col-8 textarea description").text(schedule.task[i]);
-//     var saveBtn = $("<button>").addClass("col save-btn").text("Save");
-//     taskDiv.append(taskP, taskDesc, saveBtn);
-//     $("#container").append(taskDiv);
-// };
-
-// $(".description").on("click", "textarea", function () {
-//     var text = $(this)
-//         .text()
-//         .trim();
-//     var textInput = $("<textarea>")
-//         .addClass("col-8 textarea description")
-//         .val(text);
-//     $(this).replaceWith(textInput);
-//     textInput.trigger("focus");
-// });
-
-// $(".description").on("blur", "textarea", function(){
-//     var text = $(this)
-//         .val()
-//         .trim();
-
-//     var status = $(this)
-//         .closest(".time-block")
-//         .attr("id")
-
-//     var index = $(this)
-//         .closest(".time-block")
-//         .index()
-
-//     console.log(text, status, index);
-
-//     schedule[status][index].text = text;
-
-//     localStorage.setItem("schedule", JSON.stringify(schedule));
-
-//     var taskDesc = $("<textarea>").addClass("col-8 textarea description").text(text);
-
-//     $(this).replaceWith(taskDesc);
-
-//     console.log(schedule);
-// });
+// setInterval(function(){
+//     $(".time-block").each(function(index, el) {
+//       auditTask(el);
+//     });
+//   }, (1000 * 60) * 20);
